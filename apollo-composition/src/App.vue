532ed5ef05344f5d7247013440721fe6b6680aba
Maximiliano <maximilianolencinavpc@gmail.com>
2023-07-16 11:10:38 -0300

    .

--- apollo-composition/src/App.vue
+++ apollo-composition/src/App.vue
@@ -3,6 +3,7 @@
 
   <div v-if="loading">Loading...</div>
 
+
   <div v-else>
     <ul>
       <li v-for="(comment, index) in result.getCommentsFromUser" :key="index">
@@ -16,16 +17,18 @@
   </div>
 
   <button @click="refetch()" >Refresh</button>
+  <MutationsComp />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useQuery } from "@vue/apollo-composable";
+import { useQuery, useLazyQuery } from "@vue/apollo-composable";
 import gql from "graphql-tag";
+import MutationsComp from "./components/MutationsComp.vue";
 
 export default defineComponent({
-  setup() {
-    const { result, loading, error, refetch, onResult, onError } = useQuery(gql`
+    setup() {
+        const { result, loading, error, refetch, onResult, onError } = useQuery(gql `
       query ($name: String!) {
         getCommentsFromUser(name: $name) {
           name
@@ -33,46 +36,36 @@ export default defineComponent({
         }
       }
     `, () => ({
-      //name: myReference.value
-      name: 'User 1'
-    }), {
-      // Options
-
-      //fetchPolicy: "cache-first", // Si estan cacheados los datos se los trae directamente mejor rendimiento.
-
-      //fetchPolicy: 'cache-and-network', // Va a retornar el resultado del cache-first si existe y depues va a hacer una comprobacion en la red si hay contenido disponible (mas versatil) 
-
-      //fetchPolicy: 'cache-only' // O solo 'cache' solo va a devolver los datos de la cache y si no los hay va a fallar 
-
-      //fetchPolicy: 'network-only' // Todo lo contrario a cache-only solo va a devolver los datos que hay en el servicio, nunca va a cachear
-
-      //fetchPolicy: 'no-cache' // Nunca va a cachear ese dato
-
-      fetchPolicy: 'cache-and-network',
-      pollInterval: 5000, // Por ejemplo cada 5 segundos voy a comprobar si los datos se han modificado en el servicio para traerme los datos actualizados y mostrarlos a mi usuario
-
-      
-    });
-
-    onResult((queryResult) => {
-      console.log(queryResult.data);
-      console.log(queryResult.loading);
-      console.log(queryResult.networkStatus);   
-    })
-
-    onError((error) => {
-      console.log(error.clientErrors);
-      console.log(error.graphQLErrors);
-      console.log(error.networkError);
-    })
-
-    return {
-      result,
-      loading,
-      error,
-      refetch,
-    };
-  },
+            //name: myReference.value
+            name: "User 1"
+        }), {
+            // Options
+            //fetchPolicy: "cache-first", // Si estan cacheados los datos se los trae directamente mejor rendimiento.
+            //fetchPolicy: 'cache-and-network', // Va a retornar el resultado del cache-first si existe y depues va a hacer una comprobacion en la red si hay contenido disponible (mas versatil) 
+            //fetchPolicy: 'cache-only' // O solo 'cache' solo va a devolver los datos de la cache y si no los hay va a fallar 
+            //fetchPolicy: 'network-only' // Todo lo contrario a cache-only solo va a devolver los datos que hay en el servicio, nunca va a cachear
+            //fetchPolicy: 'no-cache' // Nunca va a cachear ese dato
+            fetchPolicy: "cache-and-network",
+            pollInterval: 5000, // Por ejemplo cada 5 segundos voy a comprobar si los datos se han modificado en el servicio para traerme los datos actualizados y mostrarlos a mi usuario
+        });
+        onResult((queryResult) => {
+            console.log(queryResult.data);
+            console.log(queryResult.loading);
+            console.log(queryResult.networkStatus);
+        });
+        onError((error) => {
+            console.log(error.clientErrors);
+            console.log(error.graphQLErrors);
+            console.log(error.networkError);
+        });
+        return {
+            result,
+            loading,
+            error,
+            refetch,
+        };
+    },
+    components: { MutationsComp }
 });
 </script>
 <style>
---
+++ apollo-composition/src/components/MutationsComp.vue
@@ -0,0 +1,28 @@
+<template>
+<button @click="createComment()">Generar comentario</button>
+</template>
+
+<script lang="ts">
+import { useMutation } from "@vue/apollo-composable";
+import { defineComponent, useModel } from "vue";
+import gql from "graphql-tag";
+
+export default defineComponent({
+  setup() {
+   const { mutate: createComment } = useMutation(gql`
+      mutation ($name: String!, $text: String!) {
+        createComment(name: $name, text: $text)
+      }
+    `, () => ({
+      variables: {
+        name: 'Maximiliano',
+        text: 'Hi from vue again'
+      }
+    }) );
+
+    return {
+      createComment
+    }
+  },
+});
+</script>
